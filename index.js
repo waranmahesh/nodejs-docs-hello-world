ad7d11eedbad3a1e2f04d50f4e93c39d46f7ec87
Maheshwaran Chinnasamy <waran.mahesh@gmail.com>
2018-06-21 15:02:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World Mahesh Jenkins! EE ");
+    response.end("Hello World Mahesh Jenkins! EE TT ");
 
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
